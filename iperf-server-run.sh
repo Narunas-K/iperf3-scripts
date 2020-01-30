e29fa96797e3d79582f4ab21e75bd307f1ecee69
Narunas-K <narunas.kapocius@gmail.com>
2020-01-30 11:35:28 +0200

    Added: iperf port parameter; changed log file names; test timeframe; added test run name parameter

--- iperf-server-run.sh
+++ iperf-server-run.sh
@@ -5,10 +5,12 @@
 
 # Read arguments
 interface_ip=$1
-if [ -z $interface_ip ]
+server_port=$2
+test_name=$3
+if [ -z $interface_ip ] || [ -z $server_port ] || [ -z $test_name ]
 then
-  echo "Please provide inteface parameter. Example:"
-  echo "./iperf-server-run.sh enp3s0f0"
+  echo "Please provide inteface and port parameters. Example:"
+  echo "./iperf-server-run.sh enp3s0f0 50002 first_test"
   echo "Exiting.."
   exit
 fi
@@ -18,17 +20,17 @@ todays_date=`date +%Y%m%d-%H%M%S`
 host=`hostname -f`
 
 sar_results_dir="/tmp/phys-tcp-throughput-test/sar-results"
-sar_results_file_name=$host-$todays_date-iperfserver-sar.results
+sar_results_file_name=$host-$test_name-$todays_date-iperfserver-sar.results
 
 sar_logs_dir="/tmp/phys-tcp-throughput-test/sar-logs"
-sar_logs_file_name=$host-$todays_date-iperfserver-sar.ou
+sar_logs_file_name=$host-$test_name-$todays_date-iperfserver-sar.out
 
 iperf_results_dir="/tmp/phys-tcp-throughput-test/iperf-results"
-iperf_results_file_name=$host-$todays_date-iperfserver-iperf.results
+iperf_results_file_name=$host-$test_name-$todays_date-iperfserver-iperf.results
 
-idle_timer_before_measurement=15
-performance_measurement_timer=100
-idle_timer_after_measurement=10
+idle_timer_before_measurement=60
+performance_measurement_timer=302
+idle_timer_after_measurement=60
 
 # Script
 echo "Starting CNI/Network performance measurement run"
@@ -46,7 +48,7 @@ sleep $idle_timer_before_measurement
 # Iperf3 logging part
 echo "Starting iperf3 server"
 echo "iperf3 results are stored in $iperf_results_dir/$iperf_results_file_name"
-iperf3 -s -i $interface_ip -D --logfile $iperf_results_dir/$iperf_results_file_name
+iperf3 -s -i $interface_ip -D --logfile $iperf_results_dir/$iperf_results_file_name -p $server_port
 
 echo "going to sleep for $performance_measurement_timer seconds"
 sleep $performance_measurement_timer
