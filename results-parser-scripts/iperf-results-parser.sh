1750a4f42f3298f8327cb0616ec9c8881cfc8091
Narunas-K <narunas.kapocius@gmail.com>
2020-03-24 20:33:58 +0200

    Updated iperf results parser scripts regex

--- results-parser-scripts/iperf-results-parser.sh
+++ results-parser-scripts/iperf-results-parser.sh
@@ -9,11 +9,11 @@ current_work_dir=`pwd`
 #Read arguments
 source_dir=$1
 results_file=$2
-parse_team=$3
-if [ -z $source_dir ] || [ -z $results_file ] || [ -z $parse_team ]
+parse_regex=$3
+if [ -z $source_dir ] || [ -z $results_file ] || [ -z $parse_regex ]
 then
   echo "Please provide source data directory, results file name to which write results, and if to use regex to parse teamed interfaces results"
-  echo "./iperf-results-parser.sh source-data-dir results-file-name true"
+  echo "./iperf-results-parser.sh source-data-dir results-file-name team|kube|baremetal"
   echo "Exiting.."
   exit
 fi
@@ -28,13 +28,16 @@ echo "" > $current_work_dir/$results_file
 
 for file_name in $files_array
 do
-  if [ $parse_team == "true" ]
+  if [ $parse_regex == "team" ]
   then
     test_name=`echo "$file_name" | grep -Po "kw\d\w{4}\d_kw\d\w{4}\d_\w{8}-\d_\w{3}-\d{2,4}_\d-\d{8}-\d{6}"`
+  elif [ $parse_regex == "kube" ]
+  then
+    test_name=`echo "$file_name" | grep -Po "client\d_instance-\d_\w{3}-\d{2,4}_\d-\d{8}-\d{6}"` 
   else  
     test_name=`echo "$file_name" | grep -Po "kw\d\w{3}\d_kw\d\w{3}\d_\w{3}-\d{2,4}_\d-\d{8}-\d{6}"`
   fi
-  results=`grep "0.00-180.00" $source_dir/$file_name | grep -Po "\d+ Mbits" | grep -Po "\d+"`
+  results=`grep "0.00-120.00" $source_dir/$file_name | grep -Po "\d+ Mbits" | grep -Po "\d+"`
   results=`echo $results | tr '\n' ' '`
   printf "$test_name $results \\n" >> $current_work_dir/$results_file
 done
