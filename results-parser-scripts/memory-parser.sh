6c00575847abe4867828bb8e2380a50ab98f4a1e
Narunas-K <narunas.kapocius@gmail.com>
2020-03-24 20:32:43 +0200

    Updated memoryparser script regex to allow parse K8s results; Updated avg results calculation timeframe

--- results-parser-scripts/memory-parser.sh
+++ results-parser-scripts/memory-parser.sh
@@ -10,12 +10,12 @@ results_store_dir=`pwd`
 source_dir=$1
 results_store_dir=$2
 results_file=$3
-parse_team=$4
+parse_regex=$4
 
-if [ -z $source_dir ] || [ -z $results_store_dir ] || [ -z $results_file ] || [ -z $parse_team ]
+if [ -z $source_dir ] || [ -z $results_store_dir ] || [ -z $results_file ] || [ -z $parse_regex ]
 then
-  echo "Please provide source data directory, results store directory, file name to which write results and if to use regex to parse teamed interfaces results"
-  echo "./interrupts-parser.sh source-data-dir results-store-dir results-file-name true"
+  echo "Please provide source data directory, results store directory, file name to which write results and which regex to use for file names parser"
+  echo "./interrupts-parser.sh source-data-dir results-store-dir results-file-name team|kube|phys"
   echo "Exiting.."
   exit
 fi
@@ -31,9 +31,12 @@ files_array=`cat $results_store_dir/filesList.txt`
 
 for file_name in $files_array
 do
-  if [ $parse_team == "true" ]
+  if [ $parse_regex == "team" ]
   then
     test_name=`echo "$file_name" | grep -Po "kw\d\w{4}\d_kw\d\w{4}\d_\w{8}-\d_\w{3}-\d{2,4}_\d"`
+  elif [ $parse_regex == "kube" ]
+  then
+    test_name=`echo "$file_name" | grep -Po "kube-worker\d-\w{3}-\d{2,4}_\d-\d{8}-\d{6}"`
   else
     test_name=`echo "$file_name" | grep -Po "kw\d\w{3}\d_kw\d\w{3}\d_\w{3}-\d{2,4}_\d-\d{8}-\d{6}"`
   fi
@@ -47,15 +50,16 @@ do
   work_kbcommit_counter=0
   for value in $kbcommit_value_array
   do
-    if [ $counter -lt 60 ] || [ $counter -gt 239 ]
+    if [ $counter -lt 60 ]
     then
       idle_kbcommit_counter=`lua -e "print($idle_kbcommit_counter+$value)"`
-    else
+    elif [ $counter -gt 60 ] && [ $counter -lt 180 ]
+    then
       work_kbcommit_counter=`lua -e "print($work_kbcommit_counter+$value)"`
     fi
     counter=$(($counter+1))
   done
-  idle_kbcommit_counter_avg=`lua -e "print($idle_kbcommit_counter/125)"`
-  work_kbcommit_counter_avg=`lua -e "print($work_kbcommit_counter/180)"` 
+  idle_kbcommit_counter_avg=`lua -e "print($idle_kbcommit_counter/60)"`
+  work_kbcommit_counter_avg=`lua -e "print($work_kbcommit_counter/120)"` 
   printf "$test_name $idle_kbcommit_counter_avg $work_kbcommit_counter_avg \\n" >> $results_store_dir/$results_file
 done
