a2af77ab26ca962adf09026f5b8cd7eceb47d89f
Narunas-K <narunas.kapocius@gmail.com>
2020-03-25 21:15:50 +0200

    Updated used memory calculation algorithm

--- results-parser-scripts/memory-parser.sh
+++ results-parser-scripts/memory-parser.sh
@@ -1,33 +1,33 @@
 #/bin/sh
 # By Narunas Kapocius
 # 2020 02 16
-# Sar output binary results file interrupts parameter parser script
+# Sar output binary kbused file interrupts parameter parser script
 
 #Variables
-results_store_dir=`pwd`
+kbused_store_dir=`pwd`
 
 #Read arguments
 source_dir=$1
-results_store_dir=$2
-results_file=$3
+kbused_store_dir=$2
+kbused_file=$3
 parse_regex=$4
 
-if [ -z $source_dir ] || [ -z $results_store_dir ] || [ -z $results_file ] || [ -z $parse_regex ]
+if [ -z $source_dir ] || [ -z $kbused_store_dir ] || [ -z $kbused_file ] || [ -z $parse_regex ]
 then
-  echo "Please provide source data directory, results store directory, file name to which write results and which regex to use for file names parser"
-  echo "./interrupts-parser.sh source-data-dir results-store-dir results-file-name team|kube|phys"
+  echo "Please provide source data directory, kbused store directory, file name to which write kbused and which regex to use for file names parser"
+  echo "./interrupts-parser.sh source-data-dir kbused-store-dir kbused-file-name team|kube|phys"
   echo "Exiting.."
   exit
 fi
 
-#clean working files to prevent results overlap
-echo "" > $results_store_dir/filesList.txt
-echo "" > $results_store_dir/$results_file
+#clean working files to prevent kbused overlap
+echo "" > $kbused_store_dir/filesList.txt
+echo "" > $kbused_store_dir/$kbused_file
 
-#write sar results file list to fileList.txt
-echo -n  "$(ls $source_dir > $results_store_dir/filesList.txt)"
+#write sar kbused file list to fileList.txt
+echo -n  "$(ls $source_dir > $kbused_store_dir/filesList.txt)"
 
-files_array=`cat $results_store_dir/filesList.txt`
+files_array=`cat $kbused_store_dir/filesList.txt`
 
 for file_name in $files_array
 do
@@ -41,25 +41,35 @@ do
     test_name=`echo "$file_name" | grep -Po "kw\d\w{3}\d_kw\d\w{3}\d_\w{3}-\d{2,4}_\d-\d{8}-\d{6}"`
   fi
   echo "Processing $test_name"
-  #parse kbcached memory and write to results to file
-  results=`sadf -p $source_dir/$file_name -- -r  | grep -Po "kbcommit.\d*" | grep -Po "\d*"`
-  printf "$results\\n" > $results_store_dir/$test_name\_kbcommit.txt
-  kbcommit_value_array=`cat $results_store_dir/$test_name\_kbcommit.txt`
-  counter=0
+  #parse kbcached memory and write to kbused to file
+  kbused=`sadf -p $source_dir/$file_name -- -r  | grep -Po "kbmemused.\d*" | grep -Po "\d*"`
+  kbbuffers=`sadf -p $source_dir/$file_name -- -r  | grep -Po "kbbuffers.\d*" | grep -Po "\d*"`
+  kbcached=`sadf -p $source_dir/$file_name -- -r  | grep -Po "kbcached.\d*" | grep -Po "\d*"`
+  printf "$kbused\\n" > $kbused_store_dir/$test_name\_kbused.txt
+  printf "$kbbuffers\\n" > $kbused_store_dir/$test_name\_kbbuffers.txt
+  printf "$kbcached\\n" > $kbused_store_dir/$test_name\_kbcached.txt
+  kbused_value_array=`cat $kbused_store_dir/$test_name\_kbused.txt`
+  kbbuffers_value_array=`cat $kbused_store_dir/$test_name\_kbbuffers.txt`
+  kbcached_value_array=`cat $kbused_store_dir/$test_name\_kbcached.txt`
   idle_kbcommit_counter=0
   work_kbcommit_counter=0
-  for value in $kbcommit_value_array
+  counter_idle=0
+  counter_work=0
+  for index in {1..240} #300
   do
-    if [ $counter -lt 60 ]
+    kbused_temp=`sed -n -e "$index"p $kbused_store_dir/$test_name\_kbused.txt`
+    kbbuffers_temp=`sed -n -e "$index"p $kbused_store_dir/$test_name\_kbbuffers.txt`
+    kbcached_temp=`sed -n -e "$index"p $kbused_store_dir/$test_name\_kbcached.txt`
+    if [ $index -gt 190 ] #250
     then
-      idle_kbcommit_counter=`lua -e "print($idle_kbcommit_counter+$value)"`
-    elif [ $counter -gt 60 ] && [ $counter -lt 180 ]
+      idle_kbcommit_counter=`lua -e "print($idle_kbcommit_counter+$kbused_temp-$kbbuffers_temp-$kbcached_temp)"`
+    elif [ $index -gt 70 ] && [ $index -lt 171 ]
     then
-      work_kbcommit_counter=`lua -e "print($work_kbcommit_counter+$value)"`
+      work_kbcommit_counter=`lua -e "print($work_kbcommit_counter+$kbused_temp-$kbbuffers_temp-$kbcached_temp)"`
     fi
-    counter=$(($counter+1))
+    
   done
-  idle_kbcommit_counter_avg=`lua -e "print($idle_kbcommit_counter/60)"`
-  work_kbcommit_counter_avg=`lua -e "print($work_kbcommit_counter/120)"` 
-  printf "$test_name $idle_kbcommit_counter_avg $work_kbcommit_counter_avg \\n" >> $results_store_dir/$results_file
+  idle_kbcommit_counter_avg=`lua -e "print($idle_kbcommit_counter/50)"`
+  work_kbcommit_counter_avg=`lua -e "print($work_kbcommit_counter/100)"` 
+  printf "$test_name $idle_kbcommit_counter_avg $work_kbcommit_counter_avg \\n" >> $kbused_store_dir/$kbused_file
 done
